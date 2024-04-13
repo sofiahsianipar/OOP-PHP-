16274659e03971ab990f02ac7afbf81a619881e2
sofiahsianipar <sianiparsofiah@gmail.com>
2024-04-13 22:52:24 +0700

    Create Game.php

---
+++ oophp/autoloading/App/Produk/Game.php
@@ -0,0 +1,16 @@
+<?php
+
+class Game extends Produk implements InfoProduk {
+    public $waktuMain;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit= "penerbit", $harga= 0, $waktuMain = 100 ) {
+        parent::__construct( $judul, $penulis, $penerbit, $harga );
+        
+        $this->waktuMain = $waktuMain;
+    }
+
+    public function getInfoProduk() {
+        $str = "Game : ". $this->getInfo() ." ~ {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
